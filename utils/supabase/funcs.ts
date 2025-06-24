839f68b14d475dca11cca5fba8b15951f11772b0
ollyentwistle <ollyentwistle0@gmail.com>
2025-06-24 18:28:31 +0100

    removing old func file and removing comments from middleware

--- utils/supabase/funcs.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "./client";
-
-const supabase = createClient();
-
-export async function fetchUser() {
-  const { data, error } = await supabase.auth.getUser();
-  if (error) throw error;
-  return data.user;
-}
